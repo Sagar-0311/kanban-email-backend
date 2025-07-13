79a031185fb7e8cb3bc83b1e8c3d1d9141dfd12b
Sagar-0311 <satgpt2025@gmail.com>
2025-07-13 22:06:54 +0530

    app.py

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def send_table_email():
         return jsonify({'success': False, 'message': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=10000)
+
